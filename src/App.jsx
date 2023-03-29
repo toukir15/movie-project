49026326d98dc300853936a86d18795775b34351
Toukir Ahmed <toukir.developer.bd@gmail.com>
2023-03-30 01:57:52 +0600

    show total watch time on ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./components/Home/Home";
 import SideCart from "./components/SideCart/SideCart";
 
 const App = () => {
+  const [watchTime, setWatchTime] = useState("");
   const handleWatchTime = (time) => {
     const previousWatchTime = localStorage.getItem("watchTime");
     if (previousWatchTime) {
       const totalWatchTime = +previousWatchTime + time;
       localStorage.setItem("watchTime", totalWatchTime);
+      setWatchTime(totalWatchTime);
     } else {
       localStorage.setItem("watchTime", time);
+      watchTime(time);
     }
   };
   return (
@@ -19,7 +22,7 @@ const App = () => {
           <Home handleWatchTime={handleWatchTime}></Home>
         </div>
         <div className="flex-grow-[5] border-2 text-center">
-          <SideCart></SideCart>
+          <SideCart watchTime={watchTime}></SideCart>
         </div>
       </div>
     </>
--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const SideCart = () => {
-  return <div>this is side cart</div>;
+const SideCart = ({ watchTime }) => {
+  const [totalTime, setTotalTime] = useState(watchTime);
+  useEffect(() => {
+    const getWatchTime = localStorage.getItem("watchTime");
+    setTotalTime(getWatchTime);
+  }, [watchTime]);
+
+  return (
+    <div>
+      <h4>Total Watch Time: {totalTime}</h4>
+    </div>
+  );
 };
 
 export default SideCart;
